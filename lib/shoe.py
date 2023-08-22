07f9558c017497060b97a983a2f2ab79aecd4308
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-22 21:01:22 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        """
+        Purpose: value
+        """
+        self._size = 0
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+kiratu = Shoe("Safaris", 8)
+kiratu.size = 10
